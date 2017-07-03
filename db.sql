18f4c47c05b37c2f923f3c1d0431561e4edb81fc
chenlin <chenlin@gepingtech.com>
2017-07-03 09:14:52 +0800

    测试提交

--- db.sql
+++ db.sql
@@ -1,6 +1,9 @@
+-- hello world db.sql;
 CREATE DATABASE `eth_wallet` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 CREATE TABLE `eth_wallet`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
+
+  
