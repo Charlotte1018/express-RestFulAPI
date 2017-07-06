87363eafe2f3d59ef93bb9c2303fb97d2c154cdb
chenlin <chenlin@gepingtech.com>
2017-07-06 12:14:18 +0800

    提交数据库代码

--- db.sql
+++ db.sql
@@ -4,6 +4,9 @@ CREATE DATABASE `eth_wallet` /*!40100 DEFAULT CHARACTER SET utf8 */;
 CREATE TABLE `eth_wallet`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  
   PRIMARY KEY (`id`));
 
   
