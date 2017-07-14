4730c8943cdd61d666d848846e8876539059539a
chenlin <chenlin@gepingtech.com>
2017-07-14 17:34:50 +0800

    完工，实现了定时转账

--- js/routes/ethLightWallet.js
+++ js/routes/ethLightWallet.js
@@ -6,15 +6,15 @@ var lightwallet = require('eth-lightwallet');
 var web3Wrapper = require('../Web3Wrapper');
 var HookedWeb3Provider = require("hooked-web3-provider");
 
-router.post('/createKeyStore',function(req,res){
+router.post('/createKeyStore', function (req, res) {
     let password = req.body.password;
-    console.log("password is:",password);
+    console.log("password is:", password);
     var address0;
 
-    lightwallet.keystore.createVault({password:password},function(err,ks) {
-        if (err) throw err;        
-        
-        ks.keyFromPassword(password, function (err, pwDerivedKey) { 
+    lightwallet.keystore.createVault({ password: password }, function (err, ks) {
+        if (err) throw err;
+
+        ks.keyFromPassword(password, function (err, pwDerivedKey) {
             if (err) throw err;
             //generate eth.address;
             ks.generateNewAddress(pwDerivedKey, 3);
@@ -22,7 +22,7 @@ router.post('/createKeyStore',function(req,res){
             console.log("address0 is: ", addresses);
 
             ks.passwordProvider = function (callback) {
-            var pw = password
+                var pw = password
                 callback(null, pw);
             };
 
@@ -33,15 +33,15 @@ router.post('/createKeyStore',function(req,res){
             web3Wrapper.web3.setProvider(web3Provider);
 
             res.send([
-                {"status":"0"},
-                {"address":addresses},
-                {"ks":ks}
+                { "status": "0" },
+                { "address": addresses },
+                { "ks": ks }
             ]);
         });
-    });    
+    });
 });
 
-router.post('/sendFakeTx',function(req,res){
+router.post('/sendFakeTx', function (req, res) {
     let from = req.body.from;
     let to = req.body.to;
     let value = req.body.value;
@@ -50,15 +50,15 @@ router.post('/sendFakeTx',function(req,res){
 
     res.send({
         "message": "success",
-        "status":0,
+        "status": 0,
         "errors": {
-            "message":"",
-            "code":0
+            "message": "",
+            "code": 0
         }
     });
 })
 
-router.post('/sendTx',function(req,res){
+router.post('/sendTx', function (req, res) {
     let from = req.body.from;
     let to = req.body.to;
     let value = req.body.value;
@@ -73,69 +73,69 @@ router.post('/sendTx',function(req,res){
 
 
     web3Wrapper.web3.eth.sendTransaction({
-                from: from,
-                to: to,
-                value: value,
-                gasPrice: gasPrice,
-                gas: gas
-            }, function (err, txHash) {
-                console.log('error: ' + err);
-                console.log('txhash: ' + txHash);
-                res.send("okay");
-            });
+        from: from,
+        to: to,
+        value: value,
+        gasPrice: gasPrice,
+        gas: gas
+    }, function (err, txHash) {
+        console.log('error: ' + err);
+        console.log('txhash: ' + txHash);
+        res.send("okay");
+    });
 });
 
-router.post('/createV3',function(req,res){
+router.post('/createV3', function (req, res) {
     let password = req.body.password;
 
     var tWallet = Wallet.generate(false);
-    var wStr = tWallet.toV3(password,{
-        kdf:globalFuncs.kdf,
-        n:globalFuncs.scrypt.n
+    var wStr = tWallet.toV3(password, {
+        kdf: globalFuncs.kdf,
+        n: globalFuncs.scrypt.n
     });
     var strjson = JSON.stringify(wStr);
     var address = tWallet.getAddressString();
     var publicKey = tWallet.getPublicKeyString();
-    console.log("publicKey is: ",publicKey);
+    console.log("publicKey is: ", publicKey);
     var json = tWallet.toJSON();
-    console.log("json:",json);
-    
+    console.log("json:", json);
 
-    res.send({"strjson":strjson,"address":address});
+
+    res.send({ "strjson": strjson, "address": address });
 });
 
-router.post('/importWallet',function(req,res){
+router.post('/importWallet', function (req, res) {
     let password = req.body.password;
     let strjson = req.body.strjson;
 
-    var tWallet = Wallet.getWalletFromPrivKeyFile(strjson,password);
+    var tWallet = Wallet.getWalletFromPrivKeyFile(strjson, password);
     console.log(tWallet);
 
-    res.send({"wallet":tWallet});
+    res.send({ "wallet": tWallet });
 })
 
-router.post('/importWalletFile',function(req,res){
-    let body = req.body;    
+router.post('/importWalletFile', function (req, res) {
+    let body = req.body;
     console.log(body);
 
     let strjson = JSON.stringify(body);
     console.log(strjson);
-    res.send({"strjson":strjson});
+    res.send({ "strjson": strjson });
 })
 
 
 
 
-router.post('/fromV3',function(req,res){
+router.post('/fromV3', function (req, res) {
     let password = req.body.password;
     let strjson = req.body.strjson;
-    console.log("password: ",password);
-    console.log("strjson: ",strjson);
+    console.log("password: ", password);
+    console.log("strjson: ", strjson);
 
-    var v3Wallet = Wallet.fromV3(strjson,password,true);
+    var v3Wallet = Wallet.fromV3(strjson, password, true);
     console.log(v3Wallet);
 
-    res.send({"wallet":v3Wallet});
+    res.send({ "wallet": v3Wallet });
 });
 
 
@@ -143,15 +143,15 @@ const EthereumTx = require('ethereumjs-tx');
 const privateKey = Buffer.from('8b6b36a33897afca6111c52d8075b4e5038065c4d12a610fc43de2e87cf3f53f', 'hex')
 
 const txParams = {
-  nonce: '0x00',
-  gasPrice: '0x09184e72a000', 
-  gasLimit: '0x2710',
-  from: '0x7af5407febc9b8a511adf40499ba4662833e6dc1',
-  to: '0x0a3264926aa54756a92d84622599fc8fbd407320', 
-  value: '10000000000000000'
+    nonce: '0x00',
+    gasPrice: '0x09184e72a000',
+    gasLimit: '0x2710',
+    from: '0x7af5407febc9b8a511adf40499ba4662833e6dc1',
+    to: '0x0a3264926aa54756a92d84622599fc8fbd407320',
+    value: '10000000000000000'
 }
 
-router.post('/test002',function(req,res){
+router.post('/test002', function (req, res) {
     console.log("hello world test002");
     console.log(privateKey);
 
@@ -163,7 +163,7 @@ router.post('/test002',function(req,res){
     // var balance = web3Wrapper.web3.eth.getBalance("0x7af5407febc9b8a511adf40499ba4662833e6dc1");
     // console.log(balance);
 
-    web3Wrapper.web3.eth.sendRawTransaction(serializedTx.toString(), function(err, hash) {
+    web3Wrapper.web3.eth.sendRawTransaction(serializedTx.toString(), function (err, hash) {
         console.log(err);
         console.log(hash);
     });
@@ -172,7 +172,7 @@ router.post('/test002',function(req,res){
 });
 
 
-router.post('/test003',function(req,res){
+router.post('/test003', function (req, res) {
     console.log("hello test003");
 
     var from = "0x573f7b10e143889128EE57433B051101Ee87cF88";
@@ -181,44 +181,129 @@ router.post('/test003',function(req,res){
     var nonce = '100';
     var gasLimit = 100000;
     var gasPrice = 20000000000;
-                   
 
-    // web3Wrapper.web3.eth.sendTransaction({
-    //             from: from,
-    //             to: to,
-    //             value: value
-    //         }, function (err, txHash) {
-    //             console.log('error: ' + err);
-    //             console.log('txhash: ' + txHash);
-    //             res.send("okay");
-    //         });
 
     var Tx = require('ethereumjs-tx');
     var privateKey = Buffer.from('ec66a2866f4b93f6a02a9e53c7345eb5ba7d15ac4fa7caf0ab4a8f0b6c4e3efa', 'hex');
+    console.log("privateKey is: ", privateKey);
+    // var rawTx = {
+    //     chainId:1,
+    //     nonce:"0x97",
+    //     from:from,
+    //     to: to,
+    //     value: "0x5af3107a4000",
+    //     gasLimit: "0x5208",
+    //     gasPrice : "0x04e3b29200"
+    // };
+
+    var rawTx =
+        {
+            "nonce": "0x4c",
+            "gasPrice": "0x04e3b29200",
+            "gasLimit": "0x5208",
+            "to": "0xef3AC99B576909852D8470b219Ef1F0118b05aFc",
+            "value": "0x5af3107a4000",
+            "data": "",
+            "chainId": 1
+        };
+
+    // var jsonRawTx = JSON.stringify(rawTx);
+    var tx = new Tx(rawTx);
+    tx.sign(privateKey);
+    var serializedTx = '0x' + tx.serialize().toString('hex');
+
+
+
+    // web3Wrapper.web3.eth.sendRawTransaction("0xf86a4a8504e3b2920082520894ef3ac99b576909852d8470b219ef1f0118b05afc865af3107a40008025a0961af3c5d681a9b80d23fde96310b30f0e430f080c1bb12fe1d7c29ccbc41d9ea01ab31b6d324ba373a9822fb35c93b2caabf419db01e823d76318154cf8a9c3ad", function(err, hash){
+    //     console.log(err);
+    //     console.log(hash);
+
+    // });
+
+
+    console.log(serializedTx);
+    res.send({ "serializedTx": serializedTx });
+});
+
+router.post('/test004', function (req, res) {
+    console.log("hello test004");
+
+    var Tx = require('ethereumjs-tx');
+    var privateKey = Buffer.from('299eae76997c77adee26de25b0a3ddc332d279799a0cc05d97823fe1a943ca78', 'hex');
+    var from = "0xa075938e3af91b0988b7de2e5333416ebebe6c9f";
+    var to = "0x573f7b10e143889128ee57433b051101ee87cf88";
+
     var rawTx = {
-        chainId : 1,
-        nonce: "0x42",
-        from: from,
-        to: to,
+        chainId: "1",
+        nonce: "0x90",
+        from: "0xa075938e3af91b0988b7de2e5333416ebebe6c9f",
+        to: "0x573f7b10e143889128ee57433b051101ee87cf88",
         value: "0x5af3107a4000",
         gasLimit: "0x5208",
-        gasPrice : "0x04e3b29200"
+        gasPrice: "0x04e3b29200"
     };
-
-    var balance = web3Wrapper.web3.eth.getBalance(from);
-    console.log(balance);
 
     var tx = new Tx(rawTx);
     tx.sign(privateKey);
     var serializedTx = tx.serialize();
-    // rawTx.signedTx = '0x' + eTx.serialize().toString('hex');
-    web3Wrapper.web3.eth.sendRawTransaction('0x'+serializedTx.toString('hex'), function(err, hash){
+
+    web3Wrapper.web3.eth.sendRawTransaction("0x" + serializedTx.toString('hex'), function (err, hash) {
         console.log(err);
         console.log(hash);
-    });
 
-    res.send("okay");
-})
+        console.log(web3Wrapper.web3.eth.getBalance(from));
+        console.log(web3Wrapper.web3.eth.getBalance(to));
+    });
+    res.send("test004 okay");
+});
+
+router.post('/test005', function (req, res) {
+    console.log("hello test005");
+    var request = require('request');
+
+    var from = "0x573f7b10e143889128EE57433B051101Ee87cF88";
+    var to = "0xef3AC99B576909852D8470b219Ef1F0118b05aFc";
+
+    var Tx = require('ethereumjs-tx');
+    var privateKey = Buffer.from('ec66a2866f4b93f6a02a9e53c7345eb5ba7d15ac4fa7caf0ab4a8f0b6c4e3efa', 'hex');
+
+    let url = "https://api.etherscan.io/api?module=proxy&action=eth_getTransactionCount&address=0x573f7b10e143889128EE57433B051101Ee87cF88&tag=latest&apikey=AJKFV8KK6H6C5JRMCN4YMM9VW5AX2485JY";
+
+    var nonce;
+    request(url, function (error, res1) {
+        if (error) {res.send(error); return}
+        let body = JSON.parse(res1.body);
+        nonce = parseInt(body.result);
+        console.log("nonce is: ", nonce);
+
+        var rawTx =
+            {
+                "nonce": nonce,
+                "gasPrice": "0x04e3b29200",
+                "gasLimit": "0x5208",
+                "to": "0xef3AC99B576909852D8470b219Ef1F0118b05aFc",
+                "value": "0x5af3107a4000",
+                "data": "",
+                "chainId": 1
+            };
+
+        var tx = new Tx(rawTx);
+        tx.sign(privateKey);
+        var serializedTx = '0x' + tx.serialize().toString('hex');
+        console.log("serializedTx is: ", serializedTx);
+
+        let postUrl = "https://api.etherscan.io/api?module=proxy&action=eth_sendRawTransaction&hex="+serializedTx+"&apikey=AJKFV8KK6H6C5JRMCN4YMM9VW5AX2485JY"
+
+        console.log(postUrl);
+        // res.send("okay");
+        request.post(postUrl,function(error,res1){
+            let body = JSON.parse(res1.body);
+            let hash = body.result;
+            console.log("hash is: ",hash);
+            res.send({ "serializedTx": serializedTx,"hash":hash});
+        });
+    });
+});
 
 
 
